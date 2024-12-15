3725c59a435f7f0c51c653aca79fa978c00dcde7
raghav-karn <raghavkarn@outlook.com>
2024-12-15 10:42:16 +0530

    Scripted shown menu

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
